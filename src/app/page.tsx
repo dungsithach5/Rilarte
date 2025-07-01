36022df5778da023818f4f543e48b3b4ea7d1d1b
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-07-01 16:34:24 +0700

    #21 add new navbar on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Topbar from './layout/Topbar'
-import Sidebar from "./layout/Sidebar";
+// import Topbar from './layout/Topbar'
+import Sidebar from "./layout/BottomBar";
 
 export default function Home() {
   return (
     <div>
       <Sidebar />
-      <Topbar />
     </div>
   );
 }

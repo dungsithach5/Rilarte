271f509f484efdc91e1894757b005f536ed2f6ff
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-07-07 17:54:18 +0700

    #27 update profile

--- src/app/@/components/ProfileHeader.tsx
+++ src/app/@/components/ProfileHeader.tsx
@@ -1,5 +1,16 @@
+import { useAuth } from "../../hooks/useAuth"
 
 export default function ProfileHeader() {
+  const { session, status } = useAuth(true)
+
+  if (status === "loading") {
+    return <div>Loading...</div>
+  }
+
+  if (!session) {
+    return null
+  }
+
   return (
     <section className="w-full">      
       <img
@@ -9,15 +20,13 @@ export default function ProfileHeader() {
       />
 
       <div className="absolute top-60 left-1/2 transform -translate-x-1/2 w-48 h-48 rounded-full overflow-hidden">
-        <img
-          src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww"
-          alt="Profile"
-          className="w-full h-full object-cover"
-        />
+        {session.user?.image && (
+          <img src={session.user.image} alt="Profile" className="w-full h-full object-cover" />
+        )}
       </div>
       <div className="mt-24 text-center">
-        <h2 className="text-xl font-semibold">Ethan Walker</h2>
-        <p className="text-gray-400">@ethanwalker</p>
+        <h2 className="text-xl font-semibold">{session.user?.name}</h2>
+        <p className="text-gray-400">{session.user?.email}</p>
       </div>
 
       <div className="flex justify-center gap-4 mt-4 text-center">
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,13 @@
 "use client"
 
-import { useAuth } from "../hooks/useAuth"
+import ProfileHeader from '../@/components/ProfileHeader';
+import ProfileTabs from '../@/components/ProfileTabs';
 
 export default function ProfilePage() {
-  const { session, status } = useAuth(true)
-
-  if (status === "loading") {
-    return <div>Loading...</div>
-  }
-
-  if (!session) {
-    return null // Will redirect to auth page
-  }
-
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>Welcome {session.user?.name}</p>
-      <p>Email: {session.user?.email}</p>
-      {session.user?.image && (
-        <img src={session.user.image} alt="Profile" className="w-20 h-20 rounded-full" />
-      )}
-    </div>
+    <main className="">
+      <ProfileHeader />
+      <ProfileTabs />
+    </main>
   )
 }

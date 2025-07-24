3fb8663e2a9f131061ad22d7ffc5a1715b13c5bd
nhannguyen1208 <nhan.nguyen220402@vnuk.edu.vn>
2025-07-24 20:52:07 +0700

    #56 made opt authorized by mail

--- src/app/services/Api/register.ts
+++ src/app/services/Api/register.ts
@@ -9,3 +9,13 @@ export const registerUser = async (formData: {
   const response = await API.post('/users/register', formData)
   return response.data
 }
+
+export const sendOtp = async (email: string) => {
+  const response = await API.post('/users/send-otp', { email });
+  return response.data;
+};
+
+export const verifyOtp = async (email: string, otp: string) => {
+  const response = await API.post('/users/verify-otp', { email, otp });
+  return response.data;
+};

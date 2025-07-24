3abbe725e812341b2e24a6ae566eb81e7d3c0d51
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-07-24 08:18:39 +0700

    update file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,8 @@ export default function Home() {
   const [bannedKeywords, setBannedKeywords] = useState<string[]>([]);
   const [violation, setViolation] = useState(false);
   const { user } = useAuth(true);
+  const { session, status } = useAuth(true);
+  
 
   // Load banned keywords once
   useEffect(() => {
